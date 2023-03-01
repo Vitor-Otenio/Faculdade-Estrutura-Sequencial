48124810adbcd47e1c179a9152cff953e462f6e8
vitoro <vitor.otenio@gmail.com>
2023-02-28 21:51:24 -0300

    Test

--- Exercicios-java/src/exercicios/Ex_02.java
+++ Exercicios-java/src/exercicios/Ex_02.java
@@ -1,21 +1,22 @@
-/* 1) Faça um programa que receba dois números, calcule e mostre a subtração do primeiro
-número pelo segundo. */
+/*2) Faça um programa que receba três números, calcule e mostre a multiplicação desses números.*/
 
 package exercicios;
     import java.util.Scanner;
 
-public class Ex_01 {
+public class Ex_02 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        double n01, n02, result;
+        double n01, n02, n03, result;
         
         System.out.print("Digíte um número: ");
         n01 = input.nextDouble();
         System.out.print("Digíte um número: ");
         n02 = input.nextDouble();
+        System.out.print("Digíte um número: ");
+        n03 = input.nextDouble();
         
-        result = n01 - n02;
+        result = n01 * n02 * n03;
         
         System.out.println("\nResultado da subtração: " + result);
     }
